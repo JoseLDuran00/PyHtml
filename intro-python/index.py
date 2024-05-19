6386699e04d63e81acbe058d3cf5b4224d627270
Jose Duran <joselduran@outlook.com>
2024-05-18 21:19:10 -0400

    first commit - 18052024

---
+++ intro-python/index.py
@@ -0,0 +1,17 @@
+# Acá va un comentario
+if 5 > 3:
+    print("5 es mayor que 3")
+
+if 3 > 5:  # <- Acá va otro comentario
+    print("3 es mayor que 5")
+
+# variables
+
+x = 5
+y = "Mordedor Peludo"
+
+print(x, y)
+
+email = "correo@email.com"
+
+print(email)
